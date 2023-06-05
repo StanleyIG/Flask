59b9f627bff2c585cd8cabc34db274d0b6c72ff0
StanleyIG <oootehts@gmail.com>
2023-06-05 18:58:45 +0300

    деплой с скулайт, вообщем так я и не смог настроить postgresql в render.com

--- blog/config.py
+++ blog/config.py
@@ -4,11 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 class Config:
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     #SQLALCHEMY_DATABASE_URI = 'postgresql://myuser:mypassword@localhost:5432/blog'
     #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = 'aloaloalovotvamcluch' #os.getenv('SECRET_KEY')
     WTF_CSRF_ENABLED = True
 
 class DevelopmentConfig(Config):
