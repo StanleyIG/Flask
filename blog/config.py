ab21f855610ebb34bfaae85cd545c2ed3bcc4226
StanleyIG <oootehts@gmail.com>
2023-06-05 17:39:24 +0300

    fix7

--- blog/config.py
+++ blog/config.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 class Config:
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     #SQLALCHEMY_DATABASE_URI = 'postgresql://myuser:mypassword@localhost:5432/blog'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
     WTF_CSRF_ENABLED = True
