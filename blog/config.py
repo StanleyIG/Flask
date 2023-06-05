e853b16ca621d0f1a22ca80441e16595a507f715
StanleyIG <oootehts@gmail.com>
2023-06-05 19:40:46 +0300

    fix17

--- blog/config.py
+++ blog/config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     #SQLALCHEMY_DATABASE_URI = 'postgresql://myuser:mypassword@localhost:5432/blog'
     #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace('postgres://', 'postgresql://')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
     WTF_CSRF_ENABLED = True
